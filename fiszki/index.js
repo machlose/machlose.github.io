16f03e2a84f151e8442a5fadf49bbed4a25a5627
machlose <150077914+machlose@users.noreply.github.com>
2025-05-07 16:14:14 +0200

    test

---
+++ fiszki/index.js
@@ -0,0 +1,381 @@
+const kapitelVier = [
+    [
+        { "ger": "der Altbau", "pol": "ten stary budynek" },
+        { "ger": "der Aufzug", "pol": "winda" },
+        { "ger": "der Balkon", "pol": "balkon" },
+        { "ger": "das Bauernhaus", "pol": "dom na wsi" },
+        { "ger": "die Dachterrasse", "pol": "taras na dachu" },
+        { "ger": "die Eigentumswohnung", "pol": "mieszkanie własnościowe" },
+        { "ger": "das Einfamilienhaus", "pol": "dom jednorodzinny" },
+        { "ger": "die Etage", "pol": "piętro" },
+        { "ger": "die Garage", "pol": "garaż" },
+        { "ger": "der Gemeinschaftsgarten", "pol": "wspólny ogród" },
+        { "ger": "Hausverwaltung", "pol": "administracja domu" },
+        { "ger": "das Hochhaus", "pol": "wieżowiec" },
+        { "ger": "das Miethaus", "pol": "kamienica czynszowa, dom czynszowy" },
+        { "ger": "der Neubau", "pol": "nowy budynek, nowe budownictwo" },
+        { "ger": "das Reihenhaus", "pol": "dom szeregowy" },
+        { "ger": "der Stadtrand", "pol": "przedmieście" },
+        { "ger": "die Terrasse", "pol": "taras" },
+        { "ger": "die Tiefgarage", "pol": "garaż podziemny" },
+        { "ger": "der Umzug", "pol": "przeprowadzka" },
+        { "ger": "das Mehrfamilienhaus", "pol": "dom wielorodzinny" },
+        { "ger": "das Wohnblock", "pol": "blok mieszkalny" },
+        { "ger": "das Zweifamilienhaus", "pol": "bliźniak" }
+    ],
+    [
+        { "ger": "der Antrag", "pol": "wniosek" },
+        { "ger": "der Aushang", "pol": "wywieszka" },
+        { "ger": "die Stellenanzeige", "pol": "ogłoszenie o pracę" },
+        { "ger": "das Studentenwerk", "pol": "dział spraw studenckich" },
+        { "ger": "die Werbetafel", "pol": "plakat reklamowy" },
+        { "ger": "die Qualität", "pol": "jakość" }
+    ],
+    [
+        { "ger": "auspacken", "pol": "rozpakowywać (się)" },
+        { "ger": "beantragen", "pol": "składać wniosek o coś" },
+        { "ger": "einpacken", "pol": "pakować (się)" },
+        { "ger": "umziehen", "pol": "przeprowadzać się" }
+    ],
+    [
+        { "ger": "achten auf Akk.", "pol": "zważać / uważać na (kogoś / coś)" },
+        { "ger": "beginnen mit Dat.", "pol": "zaczynać (coś) od (czegoś)" },
+        { "ger": "sich beschweren bei Dat. über Akk.", "pol": "skarżyć się komuś na coś" },
+        { "ger": "bestehen aus Dat.", "pol": "składać się z (czegoś)" },
+        { "ger": "bitten um Akk.", "pol": "prosić o (coś)" },
+        { "ger": "helfen bei Dat.", "pol": "pomagać w (czymś)" },
+        { "ger": "hoffen auf Akk.", "pol": "mieć nadzieję na (coś)" },
+        { "ger": "schicken an Akk.", "pol": "wysyłać do (kogoś)" },
+        { "ger": "sich streiten mit Dat. über Akk.", "pol": "kłócić się z kimś o coś" },
+        { "ger": "sprechen mit Dat. über Akk.", "pol": "rozmawiać z kimś o czymś" },
+        { "ger": "vermieten an Akk.", "pol": "wynajmować (komuś)" },
+        { "ger": "warten auf Akk.", "pol": "czekać na (kogoś / coś)" },
+    ],
+    [
+        { "ger": "gezwungen sein", "pol": "być zmuszonym" },
+        { "ger": "auf höchstem Niveau", "pol": "na najwyższym poziomie" },
+        { "ger": "die Treppe nehmen", "pol": "iść po schodach" }
+    ],
+    [
+        { "ger": "das Bad putzen", "pol": "sprzątać łazienkę" },
+        { "ger": "das eigene Zimmer aufräumen", "pol": "sprzątać swój pokój" },
+        { "ger": "einkaufen", "pol": "robić zakupy" },
+        { "ger": "sich um das Haustier kümmern", "pol": "opiekować się domowym zwierzęciem" },
+        { "ger": "den Hund ausführen", "pol": "wyprowadzać psa" },
+        { "ger": "Kleidung bügeln", "pol": "prasować ubrania" },
+        { "ger": "kochen", "pol": "gotować" },
+        { "ger": "den Müll hinaustragen", "pol": "wynosić śmieci" },
+        { "ger": "staubsaugen", "pol": "odkurzać" },
+        { "ger": "Staub wischen", "pol": "ścierać kurz" },
+        { "ger": "den Tisch abräumen", "pol": "sprzątać ze stołu" },
+        { "ger": "den Boden wischen", "pol": "zmywać podłogę" },
+        { "ger": "Wäsche waschen", "pol": "prać (bieliznę), robić pranie" }
+    ],
+    [
+        {"ger": "der Betreuer", "pol": "opiekun"},
+        {"ger": "der Campus", "pol": "kampus"},
+        {"ger": "das Chaos", "pol": "bałagan"},
+        {"ger": "die Entscheidung", "pol": "decyzja"},
+        {"ger": "das Internat", "pol": "internat"},
+        {"ger": "die Nachtruhe", "pol": "cisza nocna"},
+        {"ger": "die Pension", "pol": "pensjonat"},
+        {"ger": "der Speisesaal", "pol": "jadalnia"},
+        {"ger": "das Studentenheim", "pol": "akademik"},
+        {"ger": "der Putzplan", "pol": "plan sprzątania"}
+    ],
+    [
+        {"ger": "wach bleiben", "pol": "nie spać"},
+        {"ger": "wecken", "pol": "budzić"}
+    ],
+    [
+        {"ger": "locker", "pol": "na luzie, wyluzowany (pot.)"},
+        {"ger": "streng", "pol": "surowy"}
+    ],
+    [
+        {"ger": "die Aufgabenverteilung", "pol": "podział prac / obowiązków"},
+        {"ger": "das Drogenproblem", "pol": "problem z narkotykami"},
+        {"ger": "die Eins", "pol": "jedynka (odpowiednik polskiej oceny bardzo dobrej)"},
+        {"ger": "der Familiencoach", "pol": "coach, doradca rodzinny"},
+        {"ger": "der Konflikt", "pol": "konflikt"},
+        {"ger": "die Privatsphäre", "pol": "sfera prywatna"},
+        {"ger": "die Unordnung", "pol": "bałagan, nieporządek"},
+        {"ger": "das Zusammenleben", "pol": "pożycie"}
+    ],
+    [
+        {"ger": "besorgt", "pol": "zmartwiony"},
+        {"ger": "distanziert", "pol": "zdystansowany"},
+        {"ger": "ernst", "pol": "poważny"},
+        {"ger": "sauer", "pol": "zły"},
+        {"ger": "sparsam", "pol": "oszczędny"},
+        {"ger": "traurig", "pol": "smutny"},
+        {"ger": "wütend", "pol": "wściekły"},
+        {"ger": "verschlossen", "pol": "zamknięty w sobie"}
+    ],
+    [
+        {"ger": "besetzen", "pol": "zajmować (np. łazienkę)"},
+        {"ger": "durchsuchen", "pol": "przeszukiwać"},
+        {"ger": "spaßen", "pol": "żartować"},
+        {"ger": "(sich) streiten mit Dat. über Akk.", "pol": "kłócić się z (kimś) o (coś)"},
+        {"ger": "verloren gehen", "pol": "zgubić się"},
+        {"ger": "sich wenden an Akk.", "pol": "zwracać się do (kogoś)"}
+    ]
+];
+
+const kapitelVierZdania = [
+    [
+        {"ger": "Wie findest du das alte Gartenhaus?", "pol": "Co sądzisz o starym domku ogrodowym?"},
+        {"ger": "Es gefällt mir sehr.", "pol": "Bardzo mi się podoba."},
+        {"ger": "Wie ist die neue Wohnung von Martin?", "pol": "Jakie jest nowe mieszkanie Martina?"},
+        {"ger": "Ganz schön groß.", "pol": "Całkiem duże."},
+        {"ger": "Max, hast du neue Möbel gekauft?", "pol": "Max, czy kupiłeś nowe meble?"},
+        {"ger": "Ja, den großen Schreibtisch hier.", "pol": "Tak, duże biurko tutaj."},
+        {"ger": "Musstest du dein Zimmer renovieren?", "pol": "Czy musiałeś odnowić swój pokój?"},
+        {"ger": "Ja, ich habe die Wände weiß gestrichen.", "pol": "Tak, pomalowałem ściany na biało."}
+    ],
+    [
+        {"ger": "Wo steht in deinem Zimmer das Bett?", "pol": "Gdzie w twoim pokoju stoi łóżko?"},
+        {"ger": "Unter dem Fenster.", "pol": "Pod oknem."},
+        {"ger": "Was hängt bei dir an den Wänden?", "pol": "Co wisi u ciebie na ścianach?"},
+        {"ger": "Sehr viele Poster.", "pol": "Bardzo dużo plakatów."},
+        {"ger": "Wohin legst du deine Schulsachen?", "pol": "Gdzie kładziesz swoje rzeczy szkolne?"},
+        {"ger": "Ich lege sie auf den Schreibtisch.", "pol": "Kładę je na biurku."},
+        {"ger": "Wohin kommt das neue Regal?", "pol": "Gdzie postawisz nowy regał?"},
+        {"ger": "Zwischen die Kommode und den Schreibtisch.", "pol": "Między komodą a biurkiem."}
+    ],
+    [
+        {"ger": "Hast du zu Hause Haustiere?", "pol": "Czy masz zwierzęta w domu?"},
+        {"ger": "Ja, deshalb habe ich viele Pflichten.", "pol": "Tak, dlatego mam wiele obowiązków."},
+        {"ger": "Gehst du dreimal täglich mit dem Hund Gassi?", "pol": "Czy spacerujesz trzy razy dziennie z psem?"},
+        {"ger": "Ja. Trotzdem macht mir das viel Spaß.", "pol": "Tak. Mimo to sprawia mi to wiele przyjemności."},
+        {"ger": "Katzen zu Hause? Gibt es Probleme?", "pol": "Koty w domu? Czy jest to kłopotliwe?"},
+        {"ger": "Ja, die Katzen kratzen oft an den Möbeln.", "pol": "Tak, koty drapią często meble."},
+        {"ger": "Willst du Haustiere haben?", "pol": "Czy chcesz mieć zwierzęta?"},
+        {"ger": "Ja, ein Terrarium mit Schildkröten.", "pol": "Tak, terrarium z żółwiami."}
+    ],
+    [
+        {"ger": "Ist es gut, auf dem Land zu wohnen?", "pol": "Czy dobrze jest mieszkać na wsi?"},
+        {"ger": "Ja, ich mag das langsame Leben hier.", "pol": "Tak, lubię powolne życie tutaj."},
+        {"ger": "Gefällt es dir, in der Stadt zu leben?", "pol": "Czy podoba ci się życie w mieście?"},
+        {"ger": "Ja, ich sehe nur Vorteile.", "pol": "Tak, widzę tylko zalety."},
+        {"ger": "Du lebst auf dem Land. Gibt es Nachteile?", "pol": "Mieszkasz na wsi. Czy są wady?"},
+        {"ger": "Ja, zum Kino sind es fast 35 Kilometer!", "pol": "Tak, do kina jest prawie 35 km!"},
+        {"ger": "Hast du vor, mal in einer Großstadt zu wohnen?", "pol": "Czy zamierzasz mieszkać w dużym mieście?"},
+        {"ger": "Ich frage mich das auch. Mal sehen.", "pol": "Zadaję sobie to pytanie. Zobaczymy."}
+    ]
+];
+
+const porzadkowe = [
+    {"ger": "erste", "pol": "pierwszy"},
+    {"ger": "zweite", "pol": "drugi"},
+    {"ger": "dritte", "pol": "trzeci"},
+    {"ger": "vierte", "pol": "czwarty"},
+    {"ger": "fünfte", "pol": "piąty"},
+    {"ger": "sechste", "pol": "szósty"},
+    {"ger": "siebte", "pol": "siódmy"},
+    {"ger": "achte", "pol": "ósmy"},
+    {"ger": "neunte", "pol": "dziewiąty"},
+    {"ger": "zehnte", "pol": "dziesiąty"},
+    {"ger": "elfte", "pol": "jedenasty"},
+    {"ger": "zwölfte", "pol": "dwunasty"},
+    {"ger": "dreizehnte", "pol": "trzynasty"},
+    {"ger": "vierzehnte", "pol": "czternasty"},
+    {"ger": "fünfzehnte", "pol": "piętnasty"},
+    {"ger": "sechzehnte", "pol": "szesnasty"},
+    {"ger": "siebzehnte", "pol": "siedemnasty"},
+    {"ger": "achtzehnte", "pol": "osiemnasty"},
+    {"ger": "neunzehnte", "pol": "dziewiętnasty"},
+    {"ger": "zwanzigste", "pol": "dwudziesty"}
+]
+
+let preferences = [false, false, false];
+let availableWords = create_word_array(true, true, true);
+let answer;
+
+const appDiv = document.getElementById("app");
+const resultDiv = document.getElementById("result");
+
+const lastInputSpan = document.getElementById("last-input");
+const expectedSpan = document.getElementById("output");
+const toTranslateSpan = document.getElementById("to-translate");
+
+const input = document.getElementById("input");
+const submitBtn = document.getElementById("submit");
+const okBtn = document.getElementById("ok");
+
+const opcje = document.getElementById("opcje");
+const optionsok = document.getElementById("optionsok");
+const optionsDiv = document.getElementById("options");
+const ansDiv = document.getElementById("ans");
+
+let mode = 0;
+
+function create_word_array() {
+    let array = [];
+    if (preferences[0][0]) array = array.concat(kapitelVier[0]);
+    if (preferences[0][1]) array = array.concat(kapitelVier[1]);
+    if (preferences[0][2]) array = array.concat(kapitelVier[2]);
+    if (preferences[0][3]) array = array.concat(kapitelVier[3]);
+    if (preferences[1][0]) array = array.concat(kapitelVierZdania[0]);
+    if (preferences[1][1]) array = array.concat(kapitelVierZdania[1]);
+    if (preferences[1][2]) array = array.concat(kapitelVierZdania[2]);
+    if (preferences[1][3]) array = array.concat(kapitelVierZdania[3]);
+    if (preferences[2]) array = array.concat(porzadkowe);
+    return array;
+}
+
+function apply_settings() {
+    let ok = document.querySelectorAll(".option input:checked");
+    preferences[0] = Array();
+    preferences[1] = Array();
+    preferences[2] = false;
+    ok.forEach(e => {
+        switch (e.id) {
+            case "gerpol":
+                mode = 0;
+                break;
+            case "polger":
+                mode = 1;
+                break;
+            case "random":
+                mode = 2;
+                break;
+            case "k4t1":
+                preferences[0][0] = true;
+                break;
+            case "k4t2":
+                preferences[0][1] = true;
+                break;
+            case "k4t3":
+                preferences[0][2] = true;
+                break;
+            case "k4t4":
+                preferences[0][3] = true;
+                break;
+            case "kzwei1":
+                preferences[1][0] = true;
+                break;
+            case "kzwei2":
+                preferences[1][1] = true;
+                break;
+            case "kzwei3":
+                preferences[1][2] = true;
+                break;
+            case "kzwei4":
+                preferences[1][3] = true;
+                break;
+            case "prateritum":
+                preferences[2] = true;
+                break;
+            default:
+                break;
+        }
+    });
+    availableWords = create_word_array();
+}
+
+{
+    let opts = JSON.parse(localStorage.getItem("options"));
+    if (opts !== undefined && opts !== null) {
+        for (let i = 0; i < opts.length; i++) {
+            try {
+                let e = document.getElementById(opts[i][0]);
+                e.checked = opts[i][1];
+            } catch {
+                localStorage.clear();
+                window.location.reload();
+            }
+        }
+    }
+    apply_settings();
+}
+
+function rng(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function draw_word() {
+    let randomIndex = rng(0, availableWords.length);
+    let word = availableWords[randomIndex];
+    availableWords.splice(randomIndex, 1);
+    let t = word.ger;
+    let a = word.pol;
+    if (word.pra) {
+        t = `${t} => ${word.pra}`;
+    }
+    let ok = mode;
+    if (ok === 2) {
+        ok = rng(0, 2);
+    }
+    if (ok) {
+        return [a, t];
+    } else {
+        return [t, a];
+    }
+}
+
+function submit() {
+    if (answer === undefined) {
+        answer = draw_word();
+        toTranslateSpan.textContent = answer[0];
+        return;
+    }
+    lastInputSpan.textContent = input.value;
+    ansDiv.textContent = answer[0];
+    expectedSpan.textContent = answer[1];
+    appDiv.classList.add("hidden");
+    resultDiv.classList.remove("hidden");
+}
+
+submitBtn.addEventListener("click", submit);
+
+function new_word() {
+    answer = draw_word();
+    toTranslateSpan.textContent = answer[0];
+    if (availableWords.length === 0)
+        availableWords = create_word_array(preferences[0], preferences[1], preferences[2]);
+    input.value = "";
+    appDiv.classList.remove("hidden");
+    resultDiv.classList.add("hidden");
+    input.focus();
+}
+
+okBtn.addEventListener("click", new_word);
+
+opcje.addEventListener("click", function() {
+     optionsDiv.classList.remove("hidden");
+});
+
+optionsok.addEventListener("click", function() {
+    optionsDiv.classList.add("hidden");
+    let real = document.querySelectorAll(".option input");
+    let s = [];
+    real.forEach(e => {
+         s.push([e.id, e.checked]);
+    });
+    localStorage.setItem("options", JSON.stringify(s));
+    apply_settings();
+    answer = undefined;
+    toTranslateSpan.textContent = "Kliknij przycisk aby wylosowac ok?";
+});
+
+/*
+const tenadole = document.getElementById("te-na-dole");
+document.querySelector("#tenadole").addEventListener("click", () => {
+    tenadole.classList.toggle("hidden");
+});
+*/
+
+document.getElementById("clear").addEventListener("click", function() {
+    localStorage.clear();
+    window.location.reload();
+});
+
+document.addEventListener("keypress", e => {
+    if (e.key === "Enter") {
+        if (document.activeElement === input && !appDiv.classList.contains("hidden")) {
+            submit();
+        } else if (appDiv.classList.contains("hidden")) {
+            new_word();
+        }
+    }
+});
